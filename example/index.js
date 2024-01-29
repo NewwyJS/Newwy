577dfd524f08ff6f8c532957e897f2af666f90d6
DevGier <timdegier46@gmail.com>
2024-01-29 14:15:35 +0100

    Added lib to project

---
+++ example/index.js
@@ -0,0 +1,3 @@
+import createApp from "../index.js";
+
+await createApp();
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import createApp from "../index.js";
+import { buildRoutes, buildComponents } from "./lib/build.js";
+import resetDirectory from "./lib/dir.js";
+import startServer from "./lib/server.js";
 
-await createApp();
+const port = process.env.PORT || 5173;
+
+export default async function createApp() {
+  /**
+   * Reset .newwy directory to empty
+   */
+  resetDirectory();
+
+  /**
+   * Build routes and components
+   */
+  const routes = await buildRoutes();
+  const components = await buildComponents();
+
+  /**
+   * Start server
+   */
+  startServer(port, { routes, components });
+}
